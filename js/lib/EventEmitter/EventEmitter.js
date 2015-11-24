d2b694d40f8b6b27923b5cd55a330b49ababbd90
aliakakis <aliakakis@gmail.com>
2015-11-24 19:59:52 +0200

    - Arrow functions.

--- js/lib/EventEmitter/EventEmitter.js
+++ js/lib/EventEmitter/EventEmitter.js
@@ -3,16 +3,16 @@ export default EventEmitter = (() => {
 
     return {
         // Register event
-        "on": function(eventName, callback) {
+        on: (eventName, callback) => {
             // First delete event if already registered then create again
             typeof registeredEvents[eventName] == "undefined" ? registeredEvents[eventName] = callback : (delete registeredEvents[eventName], registeredEvents[eventName] = callback);
         },
         // Broadcast event
-        "emit": function(eventName) {
+        emit: (eventName) => {
             registeredEvents[eventName]();
         },
         // Delete event
-        "off": function(eventName) {
+        off: (eventName) => {
             delete registeredEvents[eventName];
         }
     };
